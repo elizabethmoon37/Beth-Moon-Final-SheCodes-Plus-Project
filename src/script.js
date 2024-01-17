78eb8e1b71d51f1e0532a221b7cff6f9f3309615
elizabethmoon37 <elizabethmoon37@gmail.com>
2024-01-17 18:04:55 +0000

    Added Axios and API key and URL

--- src/script.js
+++ src/script.js
@@ -2,7 +2,15 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
+
+  function searchCity(city) {
+    let apiKey = "6975040fc193feta4f47d8af4o55baa9";
+    let apiUrl =
+      "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+  }
+
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchinput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
