318be66857f9baa5f2e6c6e35c7af07c53e1f213
elizabethmoon37 <elizabethmoon37@gmail.com>
2024-01-24 18:44:48 +0000

    Replaced dummy content with real time API data

--- src/script.js
+++ src/script.js
@@ -60,6 +60,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "6975040fc193feta4f47d8af4o55baa9";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -70,23 +77,28 @@ function displayForecast(response) {
   console.log(response.data);
 
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-  <div class="row">
-    <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-      <img src="" alt="" width="45" /> ☀
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+  <div class="weather-forecast-day">
+      <div class="weather-forecast-date">${formatDay(day.time)}</div>
+<img src="${day.condition.icon_url}" class="weather-forecase-icon"/>
+
       <div class="weather-forecast-temp">
-        <span class="weather-forecast-temp-max">18°</span>
-        <span class="weather-forecast-temp-min">12°</span>
-      </div>
+        <span class="weather-forecast-temp-max">${Math.round(
+          day.temperature.maximum
+        )}°</span>
+        <span class="weather-forecast-temp-min">${Math.round(
+          day.temperature.minimum
+        )}°</span>
+      
     </div>
   </div>
 `;
+    }
   });
   forecastElement.innerHTML = forecastHtml;
 }
